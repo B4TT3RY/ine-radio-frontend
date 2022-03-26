aecd2fdf60baa78cde071122097374d0f602b7d0
B4TT3RY <gb4tt3ry@gmail.com>
2022-03-27 01:36:45 +0900

    Added rounded-2xl in table

--- src/pages/dashboard/story/[id].tsx
+++ src/pages/dashboard/story/[id].tsx
@@ -42,7 +42,7 @@ export default function DashboardStoryById() {
       >
         <TableVirtuoso
           style={{ height: undefined }}
-          className='h-full shadow'
+          className='h-full shadow rounded-2xl'
           // data={regex ? stories.filter((s) => regex.test(s.content)) : stories}
           data={storyInfoId?.stories}
           components={{
