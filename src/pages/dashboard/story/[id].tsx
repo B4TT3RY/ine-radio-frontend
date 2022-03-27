4abfe8b97c7c2a55cf2fa395e6d73eaae9d33b79
B4TT3RY <gb4tt3ry@gmail.com>
2022-03-27 17:33:10 +0900

    Change color in story id

--- src/pages/dashboard/story/[id].tsx
+++ src/pages/dashboard/story/[id].tsx
@@ -42,7 +42,7 @@ export default function DashboardStoryById() {
       >
         <TableVirtuoso
           style={{ height: undefined }}
-          className='h-full shadow rounded-2xl bg-gray-100'
+          className='h-full shadow rounded-2xl bg-gray-50'
           // data={regex ? stories.filter((s) => regex.test(s.content)) : stories}
           data={storyInfoId?.stories}
           components={{
