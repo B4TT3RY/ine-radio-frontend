7d5c899baf0c3adf4d1d2e183920c051253496f6
B4TT3RY <gb4tt3ry@gmail.com>
2022-03-31 16:51:26 +0900

    Added break-all css

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -10,7 +10,7 @@ export default function Markdown({ className, children }: Props) {
   return (
     <ReactMarkdown
       rehypePlugins={[rehypeRaw]}
-      className={`dark:text-white${className ? ` ${className}` : ''}`}
+      className={`break-all dark:text-white${className ? ` ${className}` : ''}`}
       components={{
         a({ node, ...props }) {
           return (
